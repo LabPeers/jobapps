33ad252ca696405260ebf56bdd0c3a6eb832fdeb
Nikola Egetenmeyer <egetenmeyer@Nikolas-MacBook-Pro.local>
2020-10-06 17:28:43 -0500

    First commit

---
+++ backend/todo/models.py
@@ -0,0 +1,17 @@
+
+# todo/models.py
+      
+from django.db import models
+# Create your models here.
+
+# add this
+class Todo(models.Model):
+  title = models.CharField(max_length=120)
+  description = models.TextField()
+  company = models.CharField(max_length=120, default = None)
+  myLocation = models.CharField(max_length=120, default = None)
+  myDate = models.DateTimeField(auto_now=True)
+  completed = models.BooleanField(default=False)
+      
+  def __str__(self):
+    return self.title
---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,101 @@
+// frontend/src/components/Modal.js
+
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+
+export default class CustomModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: this.props.activeItem
+    };
+  }
+
+  handleChange = e => {
+    let { name, value } = e.target;
+    if (e.target.type === "checkbox") {
+      value = e.target.checked;
+    }
+    const activeItem = { ...this.state.activeItem, [name]: value };
+    this.setState({ activeItem });
+  };
+
+  render() {
+    const { toggle, onSave } = this.props;
+    return (
+      <Modal isOpen={true} toggle={toggle}>
+        <ModalHeader toggle={toggle}> Todo Item </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="title">Job Title</Label>
+              <Input
+                type="text"
+                name="title"
+                value={this.state.activeItem.title}
+                onChange={this.handleChange}
+                placeholder="Enter Todo Title"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="description">Description</Label>
+              <Input
+                type="text"
+                name="description"
+                value={this.state.activeItem.description}
+                onChange={this.handleChange}
+                placeholder="Enter Todo description"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="company">Company</Label>
+              <Input
+                type="text"
+                name="company"
+                value={this.state.activeItem.company}
+                onChange={this.handleChange}
+                placeholder="Enter company name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="myLocation">Location</Label>
+              <Input
+                type="text"
+                name="myLocation"
+                value={this.state.activeItem.myLocation}
+                onChange={this.handleChange}
+                placeholder="Enter job location"
+              />
+            </FormGroup>
+            <FormGroup check>
+              <Label for="completed">
+                <Input
+                  type="checkbox"
+                  name="completed"
+                  checked={this.state.activeItem.completed}
+                  onChange={this.handleChange}
+                />
+                Completed
+              </Label>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+            Save
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
